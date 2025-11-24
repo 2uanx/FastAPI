daba49400ab37de62ef08dc3d8950bbbfc0fd189
2uanx <tkxike@gmail.com>
2025-11-24 14:41:33 +0700

    Added class OrderResponse to add message

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ class OrderCreate(BaseModel):
     quantity: int #so luong
     price: float #gia tien
 
+class OrderResponse(BaseModel):# tao class de tra ve thong bao va don hang
+    message: str
+    order: Order
+
 order_db: List[Order] = [] #ds don hang luu tam thoi
 current_id: int = 1  # id tu tang cho cac don hang
 
@@ -26,16 +30,16 @@ current_id: int = 1  # id tu tang cho cac don hang
 
 # CREATE: tao don hang
 
-@app.post("/orders", response_model=Order, status_code=201) #khai bao duong dan
+@app.post("/orders", response_model=OrderResponse, status_code=201) #khai bao duong dan
 def create_order(order: OrderCreate): # ham va ten ham khai bao don hang moi cua khach
     global current_id # su dung bien id
     new_order = Order(id=current_id, **order.dict()) #tao don hang moi, voi id tu tao, cac thuoc tinh con lai lay tu order
     order_db.append(new_order) #them don hang moi vao db
     current_id += 1 #id tang them 1 cho don hang tiep theo
-    return {
-        "message": "Tạo đơn hàng thành công",
-        "order": new_order
-    } # tra ve don hang moi duoc khach tao
+    return OrderResponse(
+        message= "Tạo đơn hàng thành công",
+        order= new_order
+    ) # tra ve don hang moi duoc khach tao
 # READ: lay tat ca don hang
 
 @app.get("/orders", response_model=List[Order]) #khai bao duong dan lay tat ca don hang
@@ -43,28 +47,28 @@ def get_orders(): # ham va ten ham lay don
     return order_db # tra ve ds don hang
 
 # READ: lay don hang theo ID
-@app.get("/orders/{order_id}", response_model=Order) #khai bao duong dan lay don hang theo ID cua server
+@app.get("/orders/{order_id}", response_model=OrderResponse) #khai bao duong dan lay don hang theo ID cua server
 def get_order(order_id: int): # ham va ten ham lay don hang theo ID
     for order in order_db: # lay don hang trong ds 
         if order.id == order_id: # neu id don hang trung 
-            return {
-                "message": "Đon hàng được đặt thành công",
-                "order": order
-            } # tra ve don hang
+            return OrderResponse(
+                message= "Đon hàng được đặt thành công",
+                order= order
+            ) # tra ve don hang
     raise HTTPException(status_code=404, detail="Không tìm thấy đơn hàng") # kh tim thay don vi trung id
 
 # UPDATE: cap nhat don hang cua khach theo ID
 
-@app.put("/orders/{order_id}", response_model=Order) #khai bao duong dan de cap nhat don hang cua server
+@app.put("/orders/{order_id}", response_model=OrderResponse) #khai bao duong dan de cap nhat don hang cua server
 def update_order(order_id: int, updated_order: OrderCreate): #ham va ten ham cap nhat don hang cua khach da dat
     for idx, order in enumerate(order_db): #dat bien idx de lay dung don hang can cap nhat, co idx thi kh bi nham don hang
         if order.id == order_id: #trung id can upddate
             new_order = Order(id=order_id, **updated_order.dict()) # don hang moi lay id da tao va cap nhat cac thuoc tinh don hang khach dat
             order_db[idx] = new_order #ds don hang chua bien idx de cap nhat gio tro thanh don hang moi
-            return {
-                "message": "Cập nhật đã được cập nhật thành công",
-                "order": new_order
-            } # tra ve don hang da cap nhat
+            return OrderResponse(
+                message= "Đơn hàng của bạn đã được cập nhật thành công",
+                order= new_order
+            ) # tra ve don hang da cap nhat
     raise HTTPException(status_code=404, detail="Không tìm thấy đơn hàng") # neu id kh trung
 
 # DELETE: xoa don hang theo ID
